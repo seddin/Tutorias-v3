53a6c0f04fa450dca7eea1b22cd7977b6de0035c
José Ramón Jiménez Reyes <joseramon.jimenez@iesalandalus.org>
2020-02-10 19:57:02 +0100

    Esqueleto del proyecto gradle para la tarea.

---
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -0,0 +1,9 @@
+package org.iesalandalus.programacion.tutorias;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Programa para la gestión de tutorías del IES Al-Ándalus");
+	}
+
+}
