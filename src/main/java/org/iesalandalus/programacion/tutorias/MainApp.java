b66232294a41fe241a4c916f55322c04af0a0768
seddin <seddin@test.com>
2020-03-16 17:45:38 +0100

    Copiar archivos

--- src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -1,9 +1,19 @@
 package org.iesalandalus.programacion.tutorias;
 
+import javax.naming.OperationNotSupportedException;
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.tutorias.mvc.vista.IVista;
+import org.iesalandalus.programacion.tutorias.mvc.vista.texto.VistaTexto;
+
 public class MainApp {
-
-	public static void main(String[] args) {
-		System.out.println("Programa para la gestión de tutorías del IES Al-Ándalus");
-	}
-
+    public static void main(String[] args) throws OperationNotSupportedException {
+        IModelo IModelo = new Modelo(FactoriaFuenteDatos.MEMORIA.crear());
+        IVista IVista = new VistaTexto();
+        IControlador IControlador = new Controlador(IModelo, IVista);
+        IControlador.comenzar();
+    }
 }
